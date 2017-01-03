6a93efab83a56a1e7ae7283ca31a1f6fa81d04a8
jls47 <jls47@users.noreply.github.com>
2017-01-03 11:34:54 -0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,1 +1,16 @@
 // Place your FizzBuzz code here. 
+var fizzBuzz = function(){
+	for (var i = 0; i <= 20; i++){
+		if(i % 3 === 0){
+			console.log("Fizz");
+		}else if(i % 5 === 0){
+			console.log("Buzz");
+		}else if(i % 3 === 0 && i % 5 === 0){
+			console.log("Fizzbuzz");
+		}else{
+			console.log(i);
+		}
+	}
+};
+
+fizzBuzz();
