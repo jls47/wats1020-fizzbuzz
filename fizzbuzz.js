2c0c26aeaca191f4f3b54c862369b383cfa496db
jls47 <jls47@users.noreply.github.com>
2017-01-05 19:41:12 -0800

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,22 +1,29 @@
 // Place your FizzBuzz code here. 
 var fizzBuzz = function(){
 	//establishes the fizzbuzz code as a function that can be used in other objects or functions
+	var arrFizz = [];
 	for (var i = 1; i <= 20; i++){
 		//i starts at 0, adds up to 20 in increments of 1
 		if((i % 3 === 0) && (i % 5 === 0)){
 			//if i is divisible by 3 and 5 at the same time (priority):
 			console.log("Fizzbuzz");
+			arrFizz.push("Fizzbuzz");
 		}else if(i % 3 === 0){
 			//or if i is only divisible by 3
 			console.log("Fizz");
+			arrFizz.push("Fizz");
 		}else if(i % 5 === 0){
 			//or if i is only divisible by 5
 			console.log("Buzz");
+			arrFizz.push("Buzz");
 		}else{
 			//or if i does not fit any of the above criteria
 			console.log(i);
+			arrFizz.push(i);
 		}
 	}
+	stringFizz = arrFiz.join(" ");
+	return stringFizz;
 };
 
 fizzBuzz();
